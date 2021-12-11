19c4d6ef40a7fb86e898820cf67d0d66041a1604
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2021-12-11 11:30:02 -0300

    Adding lookup functionality

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Generate will generate the CLI app ready to execute
 func Generate() *cli.App {
@@ -9,5 +15,35 @@ func Generate() *cli.App {
     app.Name = "Simple IP Loopkup"
     app.Usage = "Look for IPs and nameservers on web"
 
+    app.Commands = []cli.Command{
+        {
+            Name: "ip",
+            Usage: "Look for public IP addresses on the internet",
+            Flags: []cli.Flag {
+                cli.StringFlag{
+                    Name: "host",
+                    Value: "duck.com",
+                },
+            },
+            Action: lookupIps,
+        },
+    }
+
     return app
+}
+
+func lookupIps(context *cli.Context) {
+    host := context.String("host")
+
+    fmt.Printf("Looking for %s...\n", host)
+    ips, error := net.LookupIP(host)
+
+    if error != nil {
+        log.Fatal(error)
+    }
+
+    fmt.Print("\nRESULTS::\n")
+    for _, ip := range ips {
+        fmt.Println(ip)
+    }
 }
